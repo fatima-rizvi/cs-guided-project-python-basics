d0f4f1d08361dc01cb924b4961f8b99f0993f05e
Fatima Rizvi <fatimarzv23@gmail.com>
2020-11-30 11:05:32 -0600

    Completed demo 1

--- src/demonstration_01.py
+++ src/demonstration_01.py
@@ -9,5 +9,9 @@ Examples:
 - addition(7, 3) ➞ 10
 """
 def addition(a, b):
-    # Your code here
+    sum = a + b
+    return sum
 
+print(addition(3, 2)) # 5
+print(addition(-3, -6)) # -9
+print(addition(7, 3)) # 10
